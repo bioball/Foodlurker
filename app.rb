832cf6279a0bb7d7eec24822858bfe7dad76ae49
Daniel Chao <daniel.h.chao@gmail.com>
2013-10-16 17:00:48 -0700

    Bootstrapified, and added dates to submission form

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'time'
 
 set :database, "sqlite3:///foodlurker.db"
 
@@ -12,12 +13,47 @@ class Event < ActiveRecord::Base
   belongs_to :location
 end
 
+use Rack::MethodOverride
+
 get "/" do
   @locations = Location.all
   erb :index
 end
 
 get "/:location" do
-  @events = Event.find(:all, conditions: )
+  @location = Location.find(params[:location])
+  @events = @location.events
   erb :listing
+end
+
+get "/:location/new" do
+  @location = Location.find(params[:location])
+  @event = @location.events.new
+  @today = Time.now
+  erb :newevent
+end
+
+post "/:location/new" do
+  @location = Location.find(params[:location])
+  params[:event][:starttime] = dateparser(params[:event][:starttime])
+  @event = @location.events.new(params[:event])
+  if @event.save
+    redirect "/#{@event.location.id}"
+  else
+    erb :newevent
+  end
+end
+
+delete "/:location/:event" do
+  @event = Event.find(params[:event])
+  @event.destroy
+  redirect "/#{@event.location.id}"
+end
+
+helpers do
+
+  def dateparser(date)
+    Time.parse(date)
+  end
+
 end
